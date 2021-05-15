0a6804e646e60bc2cf7c310299330b89f36dc171
Laojunge0825 <1642287765@qq.com>
2021-05-15 13:55:24 +0800

    first commit

---
+++ src/test/java/com/yc/shopconfig/ShopConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.shopconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
